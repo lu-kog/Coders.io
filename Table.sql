97aa77295238c6700287dfc7e519986245e125b3
Charu718 <160315500+Charu718@users.noreply.github.com>
2024-02-20 21:10:53 +0530

    set default for role in clanRelation

--- Table.sql
+++ Table.sql
@@ -46,7 +46,7 @@ CREATE table ClanRelation
 (
 	clanID varchar(8),
 	mailID varchar(30) NOT NULL UNIQUE,
-	role enum('CO_ADMIN', 'MEMBER'),
+	role enum('CO_ADMIN', 'MEMBER') default 'MEMBER',
 	FOREIGN KEY(clanID) REFERENCES Clan(clanID) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY(mailID) REFERENCES Login(mailID) ON DELETE CASCADE ON UPDATE CASCADE
 );
